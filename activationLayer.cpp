dfef0f0257d59934247f324d693d9f4991e5ec18
pythonprogrammer35 <100455351+pythonprogrammer35@users.noreply.github.com>
2026-01-28 00:02:06 -0800

    Update activationLayer.cpp
    I think I fixed it

--- activationLayer.cpp
+++ activationLayer.cpp
@@ -9,18 +9,53 @@
 class activationLayer{
     public:
         std::vector<std::vector<double>> input;
-        activationLayer(){}
+        std::string layerChoice;
+        //store the passed through values from sigmoid perhaps to save time for back prop
+        activationLayer(std::string desiredFunction){
+            this->layerChoice = desiredFunction;
+        }
 
         std::vector<std::vector<double>> forward(std::vector<std::vector<double>> passedMatrix) override {
             this->input = passedMatrix;
-            return(ReLU(passedMatrix));
+            if(layerChoice == "ReLU"){
+                return(ReLU(passedMatrix));
+            } else if(layerChoice == "Sigmoid"){
+                return(Sigmoid(passedMatrix));
+            }
+            
         }
 
         std::vector<std::vector<double>> backward(std::vector<std::vector<double>> backwardsInput, double learning_rate) override {
-            std::vector<std::vector<double>> holder = ReLUDeriv(this->input);
+            std::vector<std::vector<double>> holder;
+            
+            if(layerChoice == "ReLU"){
+                holder = ReLUDeriv(this->input);
+            } else if(layerChoice == "Sigmoid"){
+                holder = SigmoidDeriv(this->input);
+            }
             holder = elementMatrixMulti(holder, backwardsInput);
             return holder;
 
+        }
+
+        std::vector<std::vector<double>> Sigmoid(std::vector<std::vector<double>> passedMatrix){
+
+            for(int i = 0; i<passedMatrix.size(); i++){
+                for(int j = 0; j<passedMatrix[0].size(); j++){
+                    passedMatrix[i][j] = (1.0 /(1.0 + std::exp(-passedMatrix[i][j])));
+                }
+            }
+            return passedMatrix;
+        }
+
+        std::vector<std::vector<double>> SigmoidDeriv(std::vector<std::vector<double>> passedMatrix){
+
+            for(int i = 0; i<passedMatrix.size(); i++){
+                for(int j = 0; j<passedMatrix[0].size(); j++){
+                    passedMatrix[i][j] = (1.0 - (1.0 /(1.0 + std::exp(-passedMatrix[i][j]))));
+                }
+            }
+            return passedMatrix;
         }
         //Might try to optimize this at a later date by passing input as a reference
         std::vector<std::vector<double>> ReLU(std::vector<std::vector<double>> input){
@@ -35,6 +70,8 @@ class activationLayer{
             }
             return input;
         }
+
+
 
         std::vector<std::vector<double>> ReLUDeriv(std::vector<std::vector<double>> input){
             for(int i = 0; i<input.size(); i++){

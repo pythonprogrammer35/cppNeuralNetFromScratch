692dddd769545817a1bff88f18417a266ebd5b3d
pythonrogrammer2 <braedoncollett@gmail.com>
2026-01-28 20:21:52 -0800

    Train function fully finished

--- train.cpp
+++ train.cpp
@@ -8,22 +8,81 @@
 #include "denseLayer.cpp"
 #include "Layer.cpp"
 #include "errorCalc.cpp"
+#include <utility>
 
 
 
-/*
+
 //training loop
-void trainingLoop(std::vector<*Layer> neuralNet, std::vector<std::vector<std::vector<double>>> trainingSetX, std::vector<std::vector<std::vector<double>>> trainingSetY, int epochs, double learning_rate){
+//input becomes the output, I think i'll just have output be the default name to keep confusion to a minimum
+std::vector<std::vector<double>> passThroughNeuralNetForward(std::vector<Layer*>& neuralNet, std::vector<std::vector<double>> output){
+    double error = 0;
+    
+    for(int k = 0; k < neuralNet.size(); k++){
+        output = neuralNet[k]->forward(output);
+    }
+    return output;
 
 }
 
-passThroughNeuralNet
+std::vector<std::vector<double>> passThroughNeuralNetBackward(std::vector<Layer*>& neuralNet, std::vector<std::vector<double>> gradients, double learning_rate){
+    for(int k = neuralNet.size()-1; k>=0; k--){
+            gradients = neuralNet[k]->backward(gradients, learning_rate);
+    }
+    return gradients;
+}
 
-std::vector<std::vector<double>> testingLoop(){
+double networkTest(std::vector<Layer*>& neuralNet,  std::vector<std::vector<std::vector<double>>> trainingSetX, std::vector<std::vector<std::vector<double>>> trainingSetY) {
+    double error = 0;
+    
+    for(int i = 0; i < trainingSetX.size(); i++){
+        auto returnedOutput = passThroughNeuralNetForward(neuralNet,trainingSetX[i]);
+        error += MSE(trainingSetY[i], returnedOutput);
+    }
+    return error;
+}
+
+void trainingLoop(std::vector<Layer*>& neuralNet, std::vector<std::vector<std::vector<double>>> trainingSetX, std::vector<std::vector<std::vector<double>>> trainingSetY, int epochs, double learning_rate){
+    std::vector<std::vector<double>> output;
+    std::vector<std::vector<double>> gradients;
+    double error = 0;
+    double returnedError = 0;
+    for(int i = 0; i<epochs; i++){
+        for(int j = 0; j<trainingSetX.size(); j++){
+            output = trainingSetX[j];
+
+            //forward prop step
+            output = passThroughNeuralNetForward(neuralNet, output);
+            
+            //error and gradient calculation
+            error += MSE(trainingSetY[j], output);
+            
+
+            gradients = MSEDerivative(trainingSetY[j], output);
+            //backprop
+            passThroughNeuralNetBackward(neuralNet, gradients, learning_rate);
+        }
+
+        //print error
+        if(i % 100 == 0){
+            error = error/100;
+            std::cout << "epoch: " << i << std::endl;
+            std::cout<< "error: " << error << std::endl;
+            error = 0;
+        }
+    }
+}
+
+
+/*std::vector<std::vector<double>> testingLoop(){
 
 }*/
 
 int main(){
+    /*The hidden rule for this XOR type game is that if the index of the 
+    * first column is a 1, the output is a one. If it is a 0, the output is a zero.
+
+    */
     std::vector<std::vector<std::vector<double>>> trainingSetX = {
         {{1,0,0,0}},
         {{1,1,0,0}},
@@ -38,7 +97,7 @@ int main(){
         {{0}}
     };
 
-    int epochs = 10001;
+    int epochs = 10000;
     double learning_rate = 0.01;
 
     std::vector<Layer*> neuralNet = {
@@ -47,34 +106,23 @@ int main(){
         new Dense(10, 1), 
         new activationLayer("Sigmoid")
     };
-    std::vector<std::vector<double>> output;
-    std::vector<std::vector<double>> gradients;
-    std::vector<std::vector<double>> input;
-    double error = 0;
 
-    for(int i = 0; i<10000; i++){
-        for(int j = 0; j<trainingSetX.size(); j++){
-            output = trainingSetX[j];
-            for(int k = 0; k < neuralNet.size(); k++){
-                output = neuralNet[k]->forward(output);
-            }
-            error += MSE(trainingSetY[j],output);
+    //Modular Training Loop
+    trainingLoop(neuralNet, trainingSetX, trainingSetY, epochs, learning_rate);
+    
+    //Final pass through test
+    std::cout << "Final error:" << std::endl;
+    std::cout << (double) networkTest(neuralNet, trainingSetX, trainingSetY) << std::endl;
 
-            gradients = MSEDerivative(trainingSetY[j], output);
+    //testing portion
+    std::vector<std::vector<double>> testInput = {
+        {0,0,1,0}
+    };
 
-            for(int k = neuralNet.size()-1; k>=0; k--){
-                gradients = neuralNet[k]->backward(gradients, learning_rate);
-            }
-            
-        }
-        if(i % 100 == 0){
-            error = error/100;
-            std::cout<< "error: " << error << std::endl;
-            std::cout << "epoch: " << i << std::endl;
-            error = 0;
-        }
-    }
-    //Maybe I can add a final error readout here later
+    std::vector<std::vector<double>> returnedOutput = passThroughNeuralNetForward(neuralNet, testInput);
+
+    std::cout << "Test output" << std::endl;
+    printMatrix(returnedOutput);
 
     return 0;
 }

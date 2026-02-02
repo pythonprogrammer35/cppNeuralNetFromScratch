e2f1152cad44031312f95f5d10aa1865ec40c595
pythonrogrammer2 <braedoncollett@gmail.com>
2026-02-01 21:53:41 -0800

    Altered Layer to allow for 4D function overloads

--- Layer.cpp
+++ Layer.cpp
@@ -1,11 +1,35 @@
 #pragma once
+#include <vector>
+
+using Vector2D = std::vector<std::vector<double>>;
+using Vector3D = std::vector<std::vector<std::vector<double>>>;
+using Vector4D = std::vector<std::vector<std::vector<std::vector<double>>>>;
+
 class Layer{
     public:
         Layer(){}
 
-        virtual std::vector<std::vector<double>> forward(std::vector<std::vector<double>> input) = 0;
+        virtual Vector2D forward(Vector2D input){
+            Vector2D holder;
+            return holder;
+        };
     
-        virtual std::vector<std::vector<double>> backward(std::vector<std::vector<double>> output_gradient, double learning_rate) = 0;
+        virtual Vector2D backward(Vector2D output_gradient, double learning_rate){
+            Vector2D holder;
+            return holder;
+        }
+
+        //4D functions(for Convolution)
+        
+        virtual Vector4D forward(Vector4D input){
+            Vector4D holder;
+            return holder;
+        };
+    
+        virtual Vector4D backward(Vector4D output_gradient, double learning_rate){
+            Vector4D holder;
+            return holder;
+        }
 
         virtual std::string getName() { return "Generic Layer"; }
         virtual ~Layer() {}
